f7a89b034376a99ed0c951dc9df6f9eff6368d5c
PrakyathReddy <prakyathreddyk@gmail.com>
2023-10-03 14:54:46 +0530

    fixed typos

--- solana-intro-client/src/index.ts
+++ solana-intro-client/src/index.ts
@@ -1,32 +1,33 @@
+// We're adding these
 import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
 async function main() {
-  const connection = new Web3.Connnection(Web3.ClusterAplUrl('devnet'));
+  const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
   const signer = await initializeKeypair(connection);
 
-  console.log("PublicKey: ", signer.publicKey.toBase58());
+  console.log("Public key:", signer.publicKey.toBase58());
 }
 
 main()
-    .then(() => {
-        console.log("Finished successfully")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log('Finished successfully');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
 
 async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
   if (!process.env.PRIVATE_KEY) {
-    console.log('generating new keypair...');
+    console.log('Generating new keypair... 🗝️');
     const signer = Web3.Keypair.generate();
 
     console.log('Creating .env file');
-    fs.writeFileSync('.env', 'PRIVATE_KEY=[${signer.secretKey.toString()}]');
+    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
 
     return signer;
   }
